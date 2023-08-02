6aab712ac87ce38ee15e9b84a84b1779585eb96f
justinstoner2 <justinstoner2@gmail.com>
2023-08-02 16:17:32 -0700

    campign customers and channels all are dsplaying

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json
 from flask_navigation import Navigation
+import database.db_connector as db
 import os
 
 # Configuration
 
 app = Flask(__name__)
 nav = Navigation(app)
-
+db_connection = db.connect_to_database()
 
 #initializing Navigations
 nav.Bar('top',[
@@ -28,31 +29,52 @@ def index():
 
 @app.route('/customers')
 def customers():
-    return render_template("customers.j2")
+    query = "SELECT * FROM Customers;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("customers.j2", customers=results)
 
 @app.route('/campaigns')
 def campaigns():
-    return render_template("campaigns.j2")
+    query = "SELECT * FROM Campaigns;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("campaigns.j2", campaigns = results)
 
 @app.route('/channels')
 def channels():
-    return render_template("channels.j2")
+    query = "SELECT * FROM Channels;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("channels.j2",channels = results)
 
 @app.route('/inventory')
 def inventory():
-    return render_template("inventory.j2")
+    query = "SELECT * FROM Inventory;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("inventory.j2", inventory=results)
 
 @app.route('/products')
 def products():
-    return render_template("products.j2")
+    query = "SELECT * FROM Products;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("products.j2", products=results)
 
 @app.route('/saleItems')
 def saleItems():
-    return render_template("saleItems.j2")
+    query = "SELECT * FROM SaleItems;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("saleItems.j2", saleItems= results)
 
 @app.route('/sales')
 def sales():
-    return render_template("sales.j2")
+    query = "SELECT * FROM Sales;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("sales.j2", sales = results)
 # Listener
 
 
