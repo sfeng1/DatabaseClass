46e1decc050d60eabf0a8ff63700157ffa3206fa
Sheng Feng <fengsh@flip2.engr.oregonstate.edu>
2023-08-03 00:32:00 -0700

    Added updated select statements and CRUD functions

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, json
+from flask import Flask, render_template, json, redirect
+from flask import request 
 from flask_navigation import Navigation
 import database.db_connector as db
 import os
@@ -8,6 +9,7 @@ import os
 app = Flask(__name__)
 nav = Navigation(app)
 db_connection = db.connect_to_database()
+app.config['MYSQL_CURSORCLASS'] = "DictCursor"
 
 #initializing Navigations
 nav.Bar('top',[
@@ -29,20 +31,70 @@ def index():
 
 @app.route('/customers')
 def customers():
-    query = "SELECT * FROM Customers;"
+    query = "SELECT Customers.customerID, customerName, customerEmail, totalRevenue, (SELECT count(DISTINCT saleID) from Sales WHERE Customers.customerID = Sales.customerID) as salesCount FROM Customers JOIN (SELECT Sales.customerID, sum(quantity * productPrice) as totalRevenue FROM Sales JOIN SaleItems ON Sales.saleID = SaleItems.saleID JOIN Products ON SaleItems.productID = SaleItems.productID GROUP BY Sales.customerID) as t1 ON Customers.customerID = t1.customerID;"
     cursor = db.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()
     return render_template("customers.j2", customers=results)
 
 @app.route('/campaigns')
 def campaigns():
-    query = "SELECT * FROM Campaigns;"
+
+    # Create function for campaigns, relies on modal for input raw data
+    if request.method == "POST":
+        # used when the user presses the add campaign button
+        if request.form.get("Add_Campaign"):
+            channelID = request.form["chidinput_dd"]
+            startDate = request.form["chstartinput"]
+            endDate = request.form["chendinput"]
+            productID = request.form["pidinput_dd"]
+
+        # this mess below accounts for several possible null variataions, for sanity lets default to all fields to blank (vs 0)
+        if channelID == "" and productID == "" and endDate == "":
+            query = "INSERT INTO Campaigns (startDate) VALUES (%s);" 
+            cursor = db.execute_query(db_connection=db_connection, query=query)
+        
+        elif channelID == "" and productID == "":
+            query = "INSERT INTO Campaigns (startDate, endDate) VALUES (%s, %s);"
+            cursor = db.execute_query(db_connection=db_connection, query=query) 
+
+        elif channelID == "":
+            query = "INSERT INTO Campaigns (startDate, endDate, productID) VALUES (%s, %s, %s);" 
+            cursor = db.execute_query(db_connection=db_connection, query=query)
+
+        elif productID == "":
+            query = "INSERT INTO Campaigns (channelID, startDate, endDate) VALUES (%s, %s, %s);" 
+            cursor = db.execute_query(db_connection=db_connection, query=query)
+        
+        else: 
+            query = "INSERT INTO Campaigns (channelID, startDate, endDate, productID) VALUES (%s, %s, %s, %s);" 
+            cursor = db.execute_query(db_connection=db_connection, query=query)
+        
+        # return to product page
+        return redirect("/campaigns")
+    
+
+    query = "SELECT campaignID,  channelID, startDate, endDate, productID, (datediff(endDate, startDate) * (SELECT rate FROM Channels  WHERE Campaigns.channelID = Channels.channelID)) as cost FROM Campaigns;"
     cursor = db.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()
     return render_template("campaigns.j2", campaigns = results)
 
 @app.route('/channels')
 def channels():
+    
+    # Create function for channels, relies on modal for input raw data
+    if request.method == "POST":
+        # used when the user presses the add channel button
+        if request.form.get("Add_Channel"):
+            channelName = request.form["chnameinput"]
+            channelEmail = request.form["chemailinput"]
+            rate = request.form["chrateinput"]
+        
+        query = "INSERT INTO Channels (channelName, channelEmail, rate) VALUES (%s, %s, %s);" 
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        # return to product page
+        return redirect("/channels")
+    
+
     query = "SELECT * FROM Channels;"
     cursor = db.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()
@@ -50,13 +102,27 @@ def channels():
 
 @app.route('/inventory')
 def inventory():
-    query = "SELECT * FROM Inventory;"
+    query = "SELECT inventoryID,  productID, dateAdded, quantity, ((SELECT productPrice from Products WHERE Inventory.productID = Products.productID) * quantity) as totalValue FROM Inventory;"
     cursor = db.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()
     return render_template("inventory.j2", inventory=results)
 
 @app.route('/products')
 def products():
+    
+    # Create for products, relies on modal for input raw data
+    if request.method == "POST":
+        # used when the user presses the add product button
+        if request.form.get("Add_Product"):
+            productName = request.form["pnameinput"]
+            productPrice = request.form["ppriceinput"]
+        
+        query = "INSERT INTO Products (productName, productPrice) VALUES (%s, %s);" 
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        # return to product page
+        return redirect("/products")
+
+
     query = "SELECT * FROM Products;"
     cursor = db.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()
@@ -64,17 +130,84 @@ def products():
 
 @app.route('/saleItems')
 def saleItems():
-    query = "SELECT * FROM SaleItems;"
+    query = "SELECT saleItemID,  saleID, productID, quantity, ((SELECT productPrice from Products WHERE SaleItems.productID = Products.productID) * quantity) as totalLineItemCost FROM SaleItems;"
     cursor = db.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()
     return render_template("saleItems.j2", saleItems= results)
 
 @app.route('/sales')
 def sales():
-    query = "SELECT * FROM Sales;"
+    query = "SELECT Sales.saleID,  customerID, saleDate, totalSaleValue FROM Sales JOIN (SELECT saleID, (sum(quantity * productPrice)) as totalSaleValue FROM SaleItems JOIN Products ON SaleItems.productID = Products.productID GROUP BY saleID) as t1 ON Sales.saleID = t1.saleID;"
     cursor = db.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()
     return render_template("sales.j2", sales = results)
+
+
+# Delete route for Campaigns
+@app.route('/delete_campaign/<int:caidinput>')
+def delete_campaign(caidinput):
+    # query to delete a campaign row via caidinput passed from the delete button modal
+    query = "DELETE FROM Campaigns WHERE campaignID = '%s';"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    
+    # return to campaign page
+    return redirect("/campaigns")
+
+# Update route for Campaigns
+@app.route('/update_campaign/<int:caidinput>', methods=["Post", "Get"])
+
+def update_campaign(caidinput):
+    if request.method == "GET":
+        # query to grab the data for the campaign to be updated
+        query = "SELECT * FROM Campaigns WHERE campaignID = %s" % (caidinput)
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        results = cursor.fetchall()
+
+        # query to grab channel name data from dropdown
+        query2 = "SELECT channelID, channelName  FROM Channels"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        channel_results = cursor.fetchall()
+        
+        # query to grab product name data from dropdown
+        query3 = "SELECT productID, productName  FROM Products"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        product_results = cursor.fetchall()
+        
+        # render update_people page passing all the query data above
+        return render_template("update_people.j2", results = results, channelName = channel_results, productName = product_results)
+    
+    if request.method == "POST":
+        if request.form.get("Update_Campaign"):
+            # grab user form inputs
+            channelID = request.form["chidinput_dd"]
+            startDate = request.form["chstartinput"]
+            endDate = request.form["chendinput"]
+            productID = request.form["pidinput_dd"]
+
+        # this mess below accounts for several possible null variataions, for sanity lets default to all fields to blank (vs 0)
+        if channelID == "" and productID == "" and endDate == "":
+            query = "UPDATE Campaigns SET channelID = NULL, SET startDate = %s, SET endDate = NULL, SET productID = NULL WHERE campaignID = %s;" 
+            cursor = db.execute_query(db_connection=db_connection, query=query)
+        
+        if channelID == "" and productID == "":
+            query = "UPDATE Campaigns SET channelID = NULL, SET startDate = %s, SET endDate = %s, SET productID = NULL WHERE campaignID = %s;" 
+            cursor = db.execute_query(db_connection=db_connection, query=query)
+
+        if channelID == "":
+            query = "UPDATE Campaigns SET channelID = NULL, SET startDate = %s, SET endDate = %s, SET productID = %s WHERE campaignID = %s;" 
+            cursor = db.execute_query(db_connection=db_connection, query=query)
+
+        if productID == "":
+            query = "UPDATE Campaigns SET channelID = %s, SET startDate = %s, SET endDate = %s, SET productID = NULL WHERE campaignID = %s;" 
+            cursor = db.execute_query(db_connection=db_connection, query=query)
+        
+        else: 
+            query = "UPDATE Campaigns SET channelID = %s, SET startDate = %s, SET endDate = %s, SET productID =%s WHERE campaignID = %s;" 
+            cursor = db.execute_query(db_connection=db_connection, query=query)
+
+        # return to campaign page
+        return redirect("/campaigns")
+
 # Listener
 
 
